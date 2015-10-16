b5a3890c98a90b11cf7b39c3022d0e63e4698093
马进举 <jinjuma@yeah.net>
2015-10-16 22:23:38 +0800

    excel处理

--- engine/test-src/org/pentaho/di/trans/steps/excelinput/XlsxReader.java
+++ engine/test-src/org/pentaho/di/trans/steps/excelinput/XlsxReader.java
@@ -5,35 +5,35 @@
 * Date:2015年6月24日下午8:50:10
 * Copyright (c) 2015, jingma@iflytek.com All Rights Reserved.
 */
-/**
-* Project Name:Kettle Engine
-* File Name:ExcelUtil.java
-* Package Name:org.pentaho.di.trans.steps.excelinput
-* Date:2015年6月24日下午8:50:10
-* Copyright (c) 2015, jingma@iflytek.com All Rights Reserved.
-*/
 
 package org.pentaho.di.trans.steps.excelinput;
-import java.io.InputStream;   
-import java.util.ArrayList;   
-import java.util.Iterator;   
-import java.util.List;   
-  
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-import org.apache.poi.openxml4j.opc.OPCPackage;   
-import org.apache.poi.xssf.eventusermodel.XSSFReader;   
-import org.apache.poi.xssf.model.SharedStringsTable;   
-import org.apache.poi.xssf.usermodel.XSSFRichTextString;   
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.xssf.eventusermodel.XSSFReader;
+import org.apache.poi.xssf.model.SharedStringsTable;
+import org.apache.poi.xssf.usermodel.XSSFRichTextString;
 import org.junit.Test;
-import org.xml.sax.Attributes;   
-import org.xml.sax.InputSource;   
-import org.xml.sax.SAXException;   
-import org.xml.sax.XMLReader;   
-import org.xml.sax.helpers.DefaultHandler;   
-import org.xml.sax.helpers.XMLReaderFactory;   
+import org.pentaho.test.util.DateUtils;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
   
-public class ExcelUtil extends DefaultHandler {   
+public class XlsxReader extends DefaultHandler {   
+	/**
+	* 日志
+	*/
+	Log log = LogFactory.getLog(getClass());
        
     private SharedStringsTable sst;   
     private String lastContents;   
@@ -41,8 +41,9 @@ public class ExcelUtil extends DefaultHandler {
   
     private int sheetIndex = -1;   
     private List<String> rowlist = new ArrayList<String>();   
-    private int curRow = 0;   
-    private int curCol = 0;   
+    protected int curRow = 0;   
+    private int curCol = 0;
+	private String cellS;   
        
        
     /**  
@@ -50,8 +51,8 @@ public class ExcelUtil extends DefaultHandler {
      * @param path  
      */  
     public void readOneSheet(String path) throws Exception {   
-        OPCPackage pkg = OPCPackage.open(path);        
-        XSSFReader r = new XSSFReader(pkg);   
+        OPCPackage pkg = OPCPackage.open(path);
+        XSSFReader r = new XSSFReader(pkg);
         SharedStringsTable sst = r.getSharedStringsTable();   
                
         XMLReader parser = fetchSheetParser(sst);   
@@ -61,7 +62,8 @@ public class ExcelUtil extends DefaultHandler {
         InputSource sheetSource = new InputSource(sheet);   
         parser.parse(sheetSource);   
                
-        sheet.close();         
+        sheet.close();   
+        pkg.close();      
     }   
        
        
@@ -114,25 +116,52 @@ public class ExcelUtil extends DefaultHandler {
     public void startElement(String uri, String localName, String name,   
             Attributes attributes) throws SAXException {   
         // c => 单元格  
-        if (name.equals("c")) {   
+        if (name.equals("c")) {
             // 如果下一个元素是 SST 的索引，则将nextIsString标记为true  
+        	String cellLocation = attributes.getValue("r");  
+        	curCol = extractColumnNumber( cellLocation ) - 1; 
             String cellType = attributes.getValue("t");   
             if (cellType != null && cellType.equals("s")) {   
                 nextIsString = true;   
             } else {   
                 nextIsString = false;   
             }   
+            cellS = attributes.getValue("s");   
         }   
         // 置空   
         lastContents = "";   
     }   
-       
+    
+    public static int extractColumnNumber( String position ) {
+        int startIndex = 0;
+        while ( !Character.isDigit( position.charAt( startIndex ) ) && startIndex < position.length() ) {
+          startIndex++;
+        }
+        String colPart = position.substring( 0, startIndex );
+        return parseColumnNumber( colPart );
+      }
+
+    /**
+     * Convert the column indicator in Excel like A, B, C, AE, CX and so on to a 1-based column number.
+     * @param columnIndicator The indicator to convert
+     * @return The 1-based column number
+     */
+    public static final int parseColumnNumber( String columnIndicator ) {
+      int col = 0;
+      for ( int i = columnIndicator.length() - 1; i >= 0; i-- ) {
+        char c = columnIndicator.charAt( i );
+        int offset = 1 + Character.getNumericValue( c ) - Character.getNumericValue( 'A' );
+        col += Math.pow( 26, columnIndicator.length() - i - 1 ) * offset;
+      }
+
+      return col;
+    }
        
     public void endElement(String uri, String localName, String name)   
             throws SAXException {
         // 根据SST的索引值的到单元格的真正要存储的字符串  
         // 这时characters()方法可能会被调用多次  
-        if (nextIsString) {   
+        if (nextIsString) {
             try {   
                 int idx = Integer.parseInt(lastContents);   
                 lastContents = new XSSFRichTextString(sst.getEntryAt(idx))   
@@ -140,18 +169,32 @@ public class ExcelUtil extends DefaultHandler {
             } catch (Exception e) {   
   
             }   
-        }   
+        }
   
         // v => 单元格的值，如果单元格是字符串则v标签的值为该字符串在SST中的索引  
         // 将单元格内容加入rowlist中，在这之前先去掉字符串前后的空白符  
         if (name.equals("v")) {   
             String value = lastContents.trim();   
-            value = value.equals("") ? " " : value;   
+            value = value.equals("") ? "" : value;   
+            while(rowlist.size()<curCol){
+                rowlist.add(""); 
+            }
+            //如果是时间格式
+            if(!nextIsString&&("1".equals(cellS)||"3".equals(cellS))){
+            	 boolean date1904 = false;
+            	 try {
+					value = DateUtils.doFormatDate(
+							 DateUtil.getJavaDate(Double.parseDouble(value), date1904), 
+							 DateUtils.FORMATTER_L);
+				} catch (NumberFormatException e) {
+					log.error("不是正确的时间："+value, e);
+				}
+            }
             rowlist.add(curCol, value);   
             curCol++;   
         } else {   
             // 如果标签名称为 row ，这说明已到行尾，调用 optRows() 方法  
-            if (name.equals("row")) {   
+            if (name.equals("row")) {
                 optRow(sheetIndex, curRow, rowlist);   
                 rowlist.clear();   
                 curRow++;   
@@ -165,14 +208,18 @@ public class ExcelUtil extends DefaultHandler {
         // 得到单元格内容的值  
         lastContents += new String(ch, start, length);   
     }   
+    
+    /**
+    * 测试读取07版excel <br/>
+    * @author jingma@iflytek.com
+    */
     @Test
     public void readXlsxTest(){
-    	ExcelUtil eu = new ExcelUtil();
+    	XlsxReader eu = new XlsxReader();
     	try {
-			eu.readOneSheet("E:\\情报信息中心2015-1-19日数据提取-基本信息表.xlsx");
+			eu.readOneSheet("E:\\公租房信息.xlsx");
 		} catch (Exception e) {
 			e.printStackTrace();
-			// TODO Auto-generated catch block
 		}
     }
 }  
