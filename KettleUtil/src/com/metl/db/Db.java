8dc0b501e05718641e2f225d5fc8b82f5ccc4a86
马进举 <jinjuma@yeah.net>
2016-07-18 20:01:26 +0800

    改进数据库操作类

--- KettleUtil/src/com/metl/db/Db.java
+++ KettleUtil/src/com/metl/db/Db.java
@@ -9,7 +9,9 @@ package com.metl.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -39,6 +41,22 @@ public class Db {
     * spring数据库操作工具
     */
     private JdbcTemplate jdbcTemplate;
+    /**
+    * 获取数据库操作对象 <br/>
+    * @author jingma@iflytek.com
+    * @param dbCode
+    * @return
+    */
+    public static Db getDb(String dbCode) {
+        try {
+            DataSource dataSource = ( new DatabaseUtil() ).getNamedDataSource( dbCode );
+            JdbcTemplate jt = new JdbcTemplate(dataSource);
+            return new Db(jt);
+        } catch (KettleException e) {
+            log.error("获取数据库失败", e);
+        }
+        return null;
+    }
     /**
     * 获取数据库操作对象 <br/>
     * @author jingma@iflytek.com
@@ -81,6 +99,82 @@ public class Db {
         }
         return null;
     }
+    
+    public Db(JdbcTemplate jdbcTemplate) {
+        super();
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    /**
+    * 获取对象列表  <br/>
+    * @author jingma@iflytek.com
+    * @param sql 要执行的预编译查询语句
+    * @param prarms 参数
+    * @return
+    */
+    @SuppressWarnings("unchecked")
+    public List<JSONObject> findList(String sql,Object... prarms){
+        return (List<JSONObject>) jdbcTemplate.query(sql, prarms, new ResultSetList());
+    }
+    /**
+    * 获取对象列表  <br/>
+    * @author jingma@iflytek.com
+    * @param keyName key字段名称
+    * @param sql 要执行的预编译查询语句
+    * @param prarms 参数
+    * @return
+    */
+    public Map<String,JSONObject> findMap(String keyName,String sql,Object... prarms){
+        List<JSONObject> list = findList(sql, prarms);
+        Map<String,JSONObject> result = new HashMap<String, JSONObject>();
+        for(JSONObject jo:list){
+            result.put(jo.getString(keyName), jo);
+        }
+        return result;
+    }
+    /**
+    * 获取第一个对象 <br/>
+    * @author jingma@iflytek.com
+    * @param sql 要执行的预编译查询语句
+    * @param prarms 参数
+    * @return
+    */
+    @SuppressWarnings("unchecked")
+    public JSONObject findOne(String sql,Object... prarms){
+        List<JSONObject> list = (List<JSONObject>) jdbcTemplate.query(sql, 
+                prarms, new ResultSetList());
+        if(list.size()>0){
+            return list.get(0);
+        }else{
+            return new JSONObject();
+        }
+    }
+
+    /**
+    * 获取数据库当前时间-14位字符串 <br/>
+    * @author jingma@iflytek.com
+    * @return
+    */
+    public String getCurrentDateStr14(){
+        return findOne("select to_char(sysdate,'yyyymmddhh24miss') as current_date from dual").
+                getString("current_date");
+    }
+    /**
+    * 执行更新语句 <br/>
+    * @author jingma@iflytek.com
+    * @param sql
+    */
+    public void execute(String sql){
+        jdbcTemplate.execute(sql);
+    }
+    /**
+    * 获取数据库连接 <br/>
+    * @author jingma@iflytek.com
+    * @return
+    * @throws SQLException
+    */
+    public Connection getConn() throws SQLException{
+        return jdbcTemplate.getDataSource().getConnection();
+    }
     /**
     * 关闭数据库连接即相关预编译语句 <br/>
     * @author jingma@iflytek.com
@@ -109,59 +203,6 @@ public class Db {
                 }
             }
         }
-    }
-    
-    public Db(JdbcTemplate jdbcTemplate) {
-        super();
-        this.jdbcTemplate = jdbcTemplate;
-    }
-    /**
-    * 获取第一个对象 <br/>
-    * @author jingma@iflytek.com
-    * @param sql
-    * @param prarms
-    * @return
-    */
-    @SuppressWarnings("unchecked")
-    public List<JSONObject> findList(String sql,Object... prarms){
-        return (List<JSONObject>) jdbcTemplate.query(sql, prarms, new ResultSetList());
-    }
-
-    /**
-    * 获取数据库当前时间-14位字符串 <br/>
-    * @author jingma@iflytek.com
-    * @return
-    */
-    public String getCurrentDateStr14(){
-        return findOne("select to_char(sysdate,'yyyymmddhh24miss') as current_date from dual").
-                getString("current_date");
-    }
-    /**
-    * 执行更新语句 <br/>
-    * @author jingma@iflytek.com
-    * @param sql
-    */
-    public void execute(String sql){
-        jdbcTemplate.execute(sql);
-    }
-    /**
-    * 获取第一个对象 <br/>
-    * @author jingma@iflytek.com
-    * @param sql
-    * @param prarms
-    * @return
-    */
-    public JSONObject findOne(String sql,Object... prarms){
-        return (JSONObject) jdbcTemplate.query(sql, prarms, new ResultSetFast());
-    }
-    /**
-    * 获取数据库连接 <br/>
-    * @author jingma@iflytek.com
-    * @return
-    * @throws SQLException
-    */
-    public Connection getConn() throws SQLException{
-        return jdbcTemplate.getDataSource().getConnection();
     }
     /**
      * @return jdbcTemplate 
